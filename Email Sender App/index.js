53efc5455a8631db2c70a4e83a5312faeb350d49
tahiee <niazi.tahirkhan15@gmail.com>
2024-05-09 17:50:44 +0500

    Updates

---
+++ Email Sender App/index.js
@@ -0,0 +1,29 @@
+// Get your Own Emailjs Key 
+
+emailjs.init("PUBLIC_KEY");
+
+const sendBtn = document.querySelector('.send-btn');
+const result = document.querySelector('.result');
+
+sendBtn.addEventListener('click', sendEmail);
+
+function sendEmail() {
+    // Get the form data
+    const to = document.getElementById("to").value;
+    const subject = document.getElementById("subject").value;
+    const message = document.getElementById("message").value;
+
+    // Send the email using EmailJS
+    emailjs.send("SERVICE_ID", "TEMPLATE_ID", {
+        to_email: to,
+        subject: subject,
+        message: message
+    })
+        .then(function () {
+            result.innerHTML = "Email sent successfully!";
+            result.style.opacity = 1;
+        }, function (error) {
+            result.innerHTML = "Email sending failed!";
+            result.style.opacity = 1;
+        });
+}
